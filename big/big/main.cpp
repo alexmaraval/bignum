be03a746610819a356191d920c1637da3b451cbb
Alexandre Maraval <maraval.alexandre@gmail.com>
2017-10-06 13:51:12 +0200

    transfered class from project euler repo to this one

---
+++ big/big/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  big
+//
+//  Created by Alexandre Maraval on 06.10.17.
+//  Copyright © 2017 Alexandre Maraval. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
